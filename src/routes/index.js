0cf650c1241303dabbf007ab0aae6dea1b253d15
Ross Leitch <professional.balbatross@gmail.com>
2020-10-08 22:58:13 +1300

    router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const moniker = require('moniker')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = express.Router();
@@ -12,14 +13,16 @@ module.exports = (db) => {
   router.use(bodyParser.json())
 
   router.get('/collections', (req, res) => {
-    ModelCollection.find((err, models) => {
+    ModelCollection.find().populate('items').exec((err, models) => {
       res.send((err) ? {error: err} : models)
     })
   })
 
   router.post('/collections', (req, res) => {
     let modelCollection = new ModelCollection({
-      name: req.body.name
+      name: req.body.name,
+      items: [],
+      moniker: moniker.choose()
     })
 
     modelCollection.save((err, collection) => {
@@ -27,9 +30,33 @@ module.exports = (db) => {
     })
   })
 
-  router.get('/collection/:id', (req, res) => {
-    
-  })
+  router.route('/collections/:id')
+    .get((req, res) => {
+      ModelCollection.findById(req.params.id).populate('items').exec((err, collection) => {
+        res.send((err) ? {error: err} : collection)
+      })
+    })
+    .post((req, res) => {
+      let model = new Model({
+        name: req.body.name,
+        ipfs: req.body.ipfs,
+        tags: req.body.tags
+      })
+
+      model.save((err, _model) => {
+        ModelCollection.findById(req.params.id, (err, collection) => {
+          let items = collection.items || [];
+          items.push(_model._id)
+          ModelCollection.updateOne({
+            _id: req.params.id
+          }, {
+            items: items
+          }, {omitUndefined: true}, (err) => {
+            res.send((err) ? {error: err} : _model)
+          })
+        })
+      })
+    })
 
   router.route('/models')
     .get((req, res) => {
