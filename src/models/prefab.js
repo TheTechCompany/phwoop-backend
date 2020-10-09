1f13bdf8a2a8eba8ad9bb02dc64d662d0e60c067
Ross Leitch <professional.balbatross@gmail.com>
2020-10-09 22:28:17 +1300

    missing schema shadow

--- src/models/prefab.js
+++ src/models/prefab.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 
 const prefab = new mongoose.Schema({
   name: String,
--- src/models/world.js
+++ src/models/world.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 
 const worldSchema  = new mongoose.Schema({
   name: String,
